db52361244d97c3e5d4e0e3e3231deccb6a36a93
Sanju Raj Prasad <samblaze275@gmail.com>
2022-01-25 16:14:30 +0530

    First Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article Created Successfully!"
+        redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article Updated Successfully!"
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article Deleted Successfully!"
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "articles#index"
+  # get "/articles", to:"articles#index"
+  # get "/articles/:id", to:"articles#show"
+  resources :articles do
+    resources :comments
+  end
+end
